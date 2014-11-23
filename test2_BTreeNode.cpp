621b2fafef1420410de9d13a2847c9a5355f3e9b
Dongyang Li <dongyang.li.cs@gmail.com>
2014-11-23 01:03:34 -0800

    delete output

--- test2_BTreeNode.cpp
+++ test2_BTreeNode.cpp
@@ -1,15 +1,19 @@
 //
-//  main.cpp
-//  BTreeNode_Tests
+//  test2_BTreeNode.cpp
 //
 //  Created by Dongyang Li on 11/22/14.
 //  Copyright (c) 2014 Dongyang Li. All rights reserved.
 //
 
+#include <stdio.h>
 #include <iostream>
+#include "BTreeNode.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    BTreeLeafNode
+    
+    
     return 0;
 }
